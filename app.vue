c8a83eb0238956ca307cb29dc6de5b9b99bd980c
lanceJiang <lancejiangwei@qq.com>
2024-08-30 15:46:37 +0800

    feat: 修复通过 ElementPlus 注入 i18n 方法 无法切换语言的问题 调整

--- app.vue
+++ app.vue
@@ -1,17 +1,26 @@
 <template>
 	<div id="themeConfig" :class="themeConfig">
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
+		<el-config-provider :locale="lang">
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
+		</el-config-provider>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import Cookies from 'js-cookie'
 const themeConfig = useCookie<string>('theme-mode')
+import { messages } from '@/plugins/i18n'
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n()
 
 onMounted(() => {
 	themeConfig.value = Cookies.get('theme-mode') || 'light'
+})
+const lang = computed(() => {
+	// locale
+	return messages[locale.value] || messages['zh-CN']
 })
 </script>
 
--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
 import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+// import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.use(ElementPlus, {
-		locale: zhCn,
+		// i18n: nuxtApp.vueApp.config.globalProperties.$t
 	})
 })
