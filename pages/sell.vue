7e0f28ded0adfed181aca1803ca4d3d9a87acc85
lanceJiang <lancejiangwei@qq.com>
2024-09-05 11:43:06 +0800

    feat: sell 缩放 svg 调整

--- pages/sell.vue
+++ pages/sell.vue
@@ -62,45 +62,15 @@ import svg_p2 from '@/assets/image/sell/tiktok.svg'
 import svg_p3 from '@/assets/image/sell/woo.svg'
 import svg_p4 from '@/assets/image/sell/others.svg'
 
-import flex_1 from '@/assets/image/sourcing/flex_1.png'
-import flex_2 from '@/assets/image/sourcing/flex_2.png'
-import flex_3 from '@/assets/image/sourcing/flex_3.png'
-import flex_4 from '@/assets/image/sourcing/flex_4.png'
-import flex_5 from '@/assets/image/sourcing/flex_5.png'
-import flex_6 from '@/assets/image/sourcing/flex_6.png'
-
-const flexItems = [
-	{
-		pic: flex_1,
-		title: 'Private Label Products',
-		desc: 'E-commerce buyers who already have their own brand and product designs can use OEM to manufacture these products according to their specific requirements. The products are then sold under the buyer\'s brand name.'
-	},
-	{
-		pic: flex_2,
-		title: 'Customized Products',
-		desc: 'Buyers can modify existing products based on market demand or customer feedback, and have them produced by an OEM manufacturer. This is ideal for brands that need unique products to stand out in the market.'
-	},
-	{
-		pic: flex_3,
-		title: 'Bulk Production',
-		desc: 'Suitable for buyers looking to reduce costs through large-scale production. OEM manufacturers can provide high-quality mass production services, ensuring consistency and efficiency.'
-	},
-	{
-		pic: flex_4,
-		title: 'Rapid Product Launch',
-		desc: 'E-commerce buyers who want to quickly expand their product line but lack the time or resources for product design and development can use ODM. They can select existing designs from the manufacturer, brand them as their own, and bring them to market quickly.'
-	},
-	{
-		pic: flex_5,
-		title: 'Market Testing',
-		desc: 'Buyers can use products designed by ODM manufacturers to test new markets, assessing how the product performs without the high risk and cost of developing a new design. This approach is ideal for new brands or those exploring new categories.'
-	},
-	{
-		pic: flex_6,
-		title: 'Product Optimization',
-		desc: 'Some buyers may find that ODM designs are close to what they need and only require minor adjustments before going to market. This saves on development costs and speeds up time to market.'
-	}
-]
+// import flex_1 from '@/assets/image/sourcing/flex_1.png'
+//
+// const flexItems = [
+// 	{
+// 		pic: flex_1,
+// 		title: 'Private Label Products',
+// 		desc: 'E-commerce buyers who already have their own brand and product designs can use OEM to manufacture these products according to their specific requirements. The products are then sold under the buyer\'s brand name.'
+// 	}
+// ]
 
 const platforms = [
 	svg_p1,
@@ -156,9 +126,10 @@ const platforms = [
 			align-items: center;
 			//justify-content: space-between;
 			width: max-content;
-			gap: 102px;
+			gap: px2vw(102px);
 			.svg {
-				height: 48px;
+				font-size: px2vw(1px);
+				//height: 48px;
 			}
 			&-wrap {
 				padding: 34px 0;
