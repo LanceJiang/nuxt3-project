581bd539877f0e63c99b5b10861ab049c41a4892
lanceJiang <lancejiangwei@qq.com>
2024-09-09 10:36:55 +0800

    feat: 新增 手机端计算 & e-game活动页

---
+++ layouts/empty.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="page-container">
+		<NuxtPage />
+		<el-backtop :right="63" >
+			<el-icon class="icon-back-top"><Top /></el-icon>
+		</el-backtop>
+	</div>
+</template>
+
+<script setup>
+import { Top } from '@element-plus/icons-vue'
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+	position: relative;
+	margin: 0 auto;
+	width: 100%;
+}
+.icon-back-top {
+	color: #0A166D;
+}
+</style>
---
+++ pages/activity/e-game-word.vue
@@ -0,0 +1,382 @@
+<template>
+<div class="column-page-wrap page-wrap">
+	<div class="banner">
+		<header class="header">
+			<svg-icon name="logo_pure" class="logo" />
+			<svg-icon name="logo_txt" class="logo-text" />
+		</header>
+		<div class="banner_title">
+			Game On!
+		</div>
+		<div class="banner_txt">
+			Start Your E-commerce Journey in the Gaming World!
+		</div>
+		<div class="banner_sub">
+			Accelerate growth
+		</div>
+	</div>
+	<div class="part-1">
+		<div v-for="(group, i) of groupItems" :key="i">
+			<header class="title">
+				{{ group.title }}
+			</header>
+			<div class="box-wrap">
+				<div v-for="(v, j) of group.items" :key="j" class="item">
+					<img class="img" :src="v.img" alt="" />
+					<div class="txt">{{v.txt}}</div>
+					<div class="price">{{v.price}}</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="part-2">
+		<div class="title">Special Offer:</div>
+		<div class="sub">Start Your Dropshipping Business with Exclusive Bonuses!(Worth Over <span class="price">$500</span>)</div>
+		<div class="txt">Everything you need to start, grow, and scale your business with ease.</div>
+		<div class="sub-wrap">
+			<img src="@/assets/image/activity/e-game-word/block_1.webp" alt="" />
+			<span class="sub text-right">Done-for-You Website Setup Worth $200</span>
+		</div>
+		<div class="txt">
+			Building a high-quality, professional website from scratch can be costly and time-consuming. We take care of this for you, saving you the expense and getting you start the business quickly.
+		</div>
+		<div class="sub-wrap">
+			<img class="order-1" src="@/assets/image/activity/e-game-word/block_2.webp" alt="" />
+			<span class="sub">Product Credits Worth $150</span>
+		</div>
+		<div class="txt text-right">
+			Get credit to fulfill your first $150 worth of product costs! which is worth $300-400 in orders on your shop.
+		</div>
+		<div class="sub-wrap">
+			<img src="@/assets/image/activity/e-game-word/block_3.webp" alt="" />
+			<span class="sub">Logistics Coupon Code $150</span>
+		</div>
+		<div class="txt">
+			This logistics coupon code helps reduce your shipping expenses, letting you keep more of your revenue.
+		</div>
+	</div>
+	<div class="part-3">
+		<div class="title">Sites that we have built for our clients.</div>
+		<div class="bg-dark" >
+<!--			<img class="mac_1" src="@/assets/image/activity/e-game-word/mac1.png" alt="" />
+			<img class="mac_2" src="@/assets/image/activity/e-game-word/mac2.png" alt="" />
+			<img class="mac_3" src="@/assets/image/activity/e-game-word/mac3.png" alt="" />
+			<img class="mac_4" src="@/assets/image/activity/e-game-word/mac4.png" alt="" />
+			<img class="mac_5" src="@/assets/image/activity/e-game-word/mac5.png" alt="" />-->
+			<div class="website"></div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+	layout: 'empty'
+})
+import keyboard_1 from '@/assets/image/activity/e-game-word/keyboard_1.webp'
+import keyboard_2 from '@/assets/image/activity/e-game-word/keyboard_2.webp'
+import keyboard_3 from '@/assets/image/activity/e-game-word/keyboard_3.webp'
+import mouse_1 from '@/assets/image/activity/e-game-word/mouse_1.webp'
+import mouse_2 from '@/assets/image/activity/e-game-word/mouse_2.webp'
+import mouse_3 from '@/assets/image/activity/e-game-word/mouse_3.webp'
+import e_1 from '@/assets/image/activity/e-game-word/e_1.webp'
+import e_2 from '@/assets/image/activity/e-game-word/e_2.webp'
+import e_3 from '@/assets/image/activity/e-game-word/e_3.webp'
+import o_1 from '@/assets/image/activity/e-game-word/o_1.webp'
+import o_2 from '@/assets/image/activity/e-game-word/o_2.webp'
+import o_3 from '@/assets/image/activity/e-game-word/o_3.webp'
+const groupItems = [
+	{
+		title: 'Keyboard',
+		items: [
+			{
+				img: keyboard_1,
+				txt: 'K321 wired keyboard',
+				price: '$10.99'
+			},
+			{
+				img: keyboard_2,
+				txt: 'Qianxingzhe Mechanical ...',
+				price: '$16.99'
+			},
+			{
+				img: keyboard_3,
+				txt: 'Punk Mechanical Keyboard',
+				price: '$35.99'
+			}
+		]
+	},
+	{
+		title: 'Mouse',
+		items: [
+			{
+				img: mouse_1,
+				txt: 'PAW3995 E-sports Mouse',
+				price: '$67.99'
+			},
+			{
+				img: mouse_2,
+				txt: 'Forerunner G5 wired Mouse',
+				price: '$18.99'
+			},
+			{
+				img: mouse_3,
+				txt: 'M133 Transparent ...',
+				price: '$13.99'
+			}
+		]
+	},
+	{
+		title: 'Earphone',
+		items: [
+			{
+				img: e_1,
+				txt: 'cat ear headphones',
+				price: '$14.99'
+			},
+			{
+				img: e_2,
+				txt: 'E-sports gaming headset',
+				price: '$11.99'
+			},
+			{
+				img: e_3,
+				txt: 'Qianxingzhe X15 headset',
+				price: '$19.99'
+			}
+		]
+	},
+	{
+		title: 'Other Aecessories',
+		items: [
+			{
+				img: o_1,
+				txt: 'Fluid Rhythm Atmosphere ...',
+				price: '$57.99'
+			},
+			{
+				img: o_2,
+				txt: 'Pixel Photo Frame',
+				price: '$26.99'
+			},
+			{
+				img: o_3,
+				txt: 'Desktop Animation ...',
+				price: '$16.99'
+			}
+		]
+	}
+]
+</script>
+
+<style scoped lang="scss">
+//mx2vw()
+.page-wrap {
+	font-family: "Nunito Sans";
+	background: #16071C;
+	color: #fff;
+	//overflow-x: hidden;
+	.banner {
+		//position: absolute;
+		width: 100%;
+		height: mx2vw(312px);
+		background: url('@/assets/image/activity/e-game-word/banner.webp') no-repeat top center;
+		background-size: 100% 100%;
+		margin-bottom: mx2vw(-6px);
+		font-size: mx2vw(16px);
+		text-transform: capitalize;
+		padding-left: mx2vw(26px);
+		&_title {
+			padding-top: mx2vw(58px);
+			font-size: mx2vw(36px);
+			line-height: mx2vw(56px);
+			font-weight: 700;
+		}
+		&_txt {}
+		&_sub {
+			margin-top: mx2vw(6px);
+			color: #FFC300;
+			//font-weight: 600;
+			font-style: italic;
+		}
+	}
+	.header {
+		display: flex;
+		align-items: center;
+		height: mx2vw(60px);
+		//padding-left: mx2vw(20px);
+		margin-left: mx2vw(-6px);
+		background: rgba(22, 7, 28, 0.50);
+		.logo {
+			font-size: mx2vw(39px);
+			&-text {
+				margin-left: mx2vw(12px);
+				font-size: mx2vw(58px);
+				//height: mx2vw(8px);
+			}
+		}
+	}
+	.part-1 {
+		padding: mx2vw(24px) mx2vw(16px);
+		display: flex;
+		flex-direction: column;
+		gap: mx2vw(36px);
+		background: #EDEDED;
+		color: #212121;
+		.title {
+			padding-bottom: mx2vw(16px);
+			font-weight: 600;
+			font-size: mx2vw(16px);
+			text-transform: capitalize;
+		}
+		.box-wrap {
+			display: flex;
+			align-items: center;
+			gap: mx2vw(9px);
+			.item {
+				width: mx2vw(108px);
+				font-size: mx2vw(12px);
+				.img {
+					width: 100%;
+					height: mx2vw(108px);
+					border-radius: mx2vw(6px);
+					background: #fff;
+				}
+				.txt {
+					margin: mx2vw(8px) 0;
+					font-weight: 700;
+					text-transform: capitalize;
+				}
+				.price {
+					color: #FF4626;
+					font-size: mx2vw(16px);
+					font-weight: 700;
+				}
+			}
+		}
+	}
+	.part-2 {
+		margin-top: mx2vw(40px);
+		padding: mx2vw(30px) mx2vw(16px);
+		height: mx2vw(692px);
+		font-size: mx2vw(12px);
+		text-transform: capitalize;
+		color: #AAA;
+		background: url('@/assets/image/activity/e-game-word/bg_2.webp') no-repeat top center;
+		background-size: 100% 100%;
+		.title {
+			font-size: mx2vw(24px);
+			font-weight: 700;
+			color: #fff;
+		}
+		.sub {
+			flex: 1;
+			margin-top: mx2vw(10px);
+			color: #fff;
+			font-size: mx2vw(14px);
+			font-weight: 500;
+			&-wrap {
+				display: flex;
+				align-items: center;
+				margin: mx2vw(12px) 0;
+				img {
+					width: mx2vw(80px);
+					margin: 0 mx2vw(18px);
+				}
+			}
+		}
+		.price {
+			font-size: mx2vw(24px);
+			font-style: italic;
+			font-weight: 700;
+			color: #FFC300;
+		}
+		.txt {
+			margin-top: mx2vw(12px);
+		}
+	}
+	.part-3 {
+		overflow-x: hidden;
+		padding-bottom: mx2vw(90px);
+		.bg-dark {
+			position: relative;
+			width: mx2vw(409px);
+			height: mx2vw(1004px);
+			margin-left: mx2vw(-13px);
+			margin-right: mx2vw(-21px);
+			background: url('@/assets/image/activity/e-game-word/part-3_bg_1.webp') no-repeat top center;
+			background-size: 100% 100%;
+			padding-left: mx2vw(20px);
+		}
+		/*.mac {
+			&_1 {
+				width: mx2vw(274px);
+				//margin-top: mx2vw(-108px);
+				margin-top: mx2vw(-110px);
+			}
+			&_2 {
+				width: mx2vw(218px);
+			}
+			&_3 {
+				width: mx2vw(251px);
+			}
+			&_4 {
+				width: mx2vw(216px);
+			}
+			&_5 {
+				width: mx2vw(224px);
+			}
+		}*/
+		.website {
+			position: absolute;
+			left: mx2vw(25px);
+			top: mx2vw(-107px);
+			width: mx2vw(321px);
+			height: mx2vw(1060px);
+			background: url('@/assets/image/activity/e-game-word/website.png') no-repeat top center;
+			background-size: 100% 100%;
+		}
+		font-size: mx2vw(12px);
+		text-transform: capitalize;
+		color: #AAA;
+		.title {
+			margin: mx2vw(40px) mx2vw(16px);
+			padding-bottom: mx2vw(108px);
+			font-size: mx2vw(24px);
+			font-style: italic;
+			font-weight: 700;
+			color: #fff;
+			text-align: center;
+		}
+/*
+		.sub {
+			flex: 1;
+			margin-top: mx2vw(10px);
+			color: #fff;
+			font-size: mx2vw(14px);
+			font-weight: 500;
+			&-wrap {
+				display: flex;
+				align-items: center;
+				margin: mx2vw(12px) 0;
+				img {
+					width: mx2vw(80px);
+					margin: 0 mx2vw(18px);
+				}
+			}
+		}
+		.price {
+			font-size: mx2vw(24px);
+			font-style: italic;
+			font-weight: 700;
+			color: #FFC300;
+		}
+		.txt {
+			margin-top: mx2vw(12px);
+		}*/
+	}
+	.pxvw-16 {
+		padding: 0 mx2vw(16px);
+	}
+}
+</style>
