485db6cc2d9d4f038315a20314b56994501b2d1b
lanceJiang <lancejiangwei@qq.com>
2024-09-10 14:58:27 +0800

    feat: 手机端屏幕 windi.config.ts配置

--- windi.config.ts
+++ windi.config.ts
@@ -11,12 +11,24 @@ export default {
 		"./error.vue",
 	],
 	theme: {
-		// screens: {
-		// 	sm: '480px',
-		// 	md: '768px',
-		// 	lg: '976px',
-		// 	xl: '1440px'
-		// },
+		screens: {
+			'mobile': { max: '750px' }, // 最大宽 750
+			'min-m': '750px', // 最小宽 640
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1440px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px'
+			// => @media (min-width: 1536px) { ... }
+		},
 		// colors: {
 		// 	// blue: '#1fb6ff',
 		// 	// pink: '#ff49db',
