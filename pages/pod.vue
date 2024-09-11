9be344b4080a538d72fc2e61e48db92cc1064fd3
lanceJiang <lancejiangwei@qq.com>
2024-09-11 14:10:36 +0800

    feat: 手机端 pod 页面处理

--- pages/pod.vue
+++ pages/pod.vue
@@ -4,15 +4,17 @@
 			<div class="part-title" style="padding-top: 40px;">
 				Print On Demand
 			</div>
-			<div class="text-center">
+			<div class="text-center mobile:mb-[24px]">
 				Start Your Online Business with Ease Create and sell custom products with just one click.
 			</div>
-			<div class="mt-[16px] mb-[24px] text-center">
+			<div class="mobile:hidden mt-[16px] mb-[24px] text-center">
 				<el-button class="min-w-[230px]" type="primary" round @click="localJump">Start Selling</el-button>
 			</div>
 		</div>
 		<img class="video-demo" src="~@/assets/image/pod/banner.webp" />
-
+		<div class="pc:hidden mt-[16px] text-center">
+			<el-button class="min-w-[230px]" type="primary" round @click="localJump">Start Selling</el-button>
+		</div>
 		<!--demand-->
 		<div class="common-inner-auto">
 			<div class="part-title">
@@ -21,7 +23,7 @@
 			<div class="ladder-wrap">
 				<div v-for="(v, i) of ladders" :key="i" class="ladder_item" :style="`padding-left: ${180 * i}px`">
 					<img :src="v.pic" />
-					<div class="desc-wrap">
+					<div class="desc-wrap mobile:text-center">
 						<header class="capitalize">{{ v.title }}</header>
 						<p class="desc" v-for="(desc, _i) of v.descs" :key="_i">{{desc}}</p>
 					</div>
@@ -34,24 +36,24 @@
 			<div class="part-title">
 				Turn your creativity into profit with our print-on-demand services.
 			</div>
-			<div class="text-center">
+			<div class="services-tip text-center">
 				Whether you're an artist, entrepreneur, or creative enthusiast, we make it easy for you to launch your own brand and connect with your followers.
 			</div>
-			<div class="services-wrap mt-[30px]">
+			<div class="services-wrap">
 				<div class="item">
 					<img src="@/assets/image/pod/services_1.webp" />
 					<div class="desc-wrap">
 						<header class="title">Creators and Designers</header>
-						<p class="mt-[16px] mb-[40px]">Start monetizing your audience or art with zero upfront investment. Selling custom merchandise is the best way to keep your creative passion alive.</p>
+						<p class="desc">Start monetizing your audience or art with zero upfront investment. Selling custom merchandise is the best way to keep your creative passion alive.</p>
 						<el-button type="primary" round class="min-w-[230px]" @click="localJump">Creat Your Merch</el-button>
 					</div>
 				</div>
 
-				<div class="item mt-[20px]">
+				<div class="item item-2 mt-[20px]">
 					<img class="order-2" src="@/assets/image/pod/services_2.webp" />
 					<div class="desc-wrap text-right">
 						<header class="title">Ecommerce Entrepreneurs</header>
-						<p class="mt-[16px] mb-[40px]">Build your brand without any upfront costs. Launching or growing your e-commerce business is now easier than ever with Hyinsight</p>
+						<p class="desc">Build your brand without any upfront costs. Launching or growing your e-commerce business is now easier than ever with Hyinsight</p>
 						<el-button type="primary" round class="min-w-[230px]" @click="localJump">Start Selling</el-button>
 					</div>
 				</div>
@@ -63,56 +65,72 @@
 			<div class="part-title">
 				How Hyinsight empower your E-commerce growth
 			</div>
-			<div class="growth-wrap -mt-[30px]">
-				<div class="item items-end">
-					<img class="transform translate-y-[50px]" src="@/assets/image/pod/growth_1.webp" />
-					<div class="desc-wrap">
-						<header class="title"><svg_gift class="icon"/> Endless Product Selection – Create Your Signature Line</header>
-						<p class="mt-[12px]">
-							<span class="font-bold">Tailored for Your Brand:</span>
-							Choose from a wide array of products like apparel, accessories, and more, giving you the flexibility to create a unique merchandise line that resonates with your customers.
-						</p>
-						<p class="mt-[12px]">
-							<span class="font-bold">Premium Quality:</span>
-							Stand out with high-quality products that reflect your brand’s values and ensure customers love what they buy.
-						</p>
+			<div class="growth-wrap">
+				<div class="item-box">
+					<header class="title pc:hidden"><svg-icon name="gift" class="icon" />Endless Product Selection – Create Your Signature Line</header>
+					<div class="item pc:items-end pc:-mt-[30px]">
+						<img class="transform translate-y-[50px]" src="@/assets/image/pod/growth_1.webp" />
+						<div class="desc-wrap">
+							<header class="title mobile:hidden"><svg-icon name="gift" class="icon" /> Endless Product Selection – Create Your Signature Line</header>
+							<p class="desc">
+								<span class="font-bold">Tailored for Your Brand:</span>
+								Choose from a wide array of products like apparel, accessories, and more, giving you the flexibility to create a unique merchandise line that resonates with your customers.
+							</p>
+							<p class="desc">
+								<span class="font-bold">Premium Quality:</span>
+								Stand out with high-quality products that reflect your brand’s values and ensure customers love what they buy.
+							</p>
+						</div>
 					</div>
 				</div>
 
-				<div class="item items-start mt-[50px]">
-					<img class="order-2 transform -translate-y-[20px]" src="@/assets/image/pod/growth_2.webp" />
-					<div class="desc-wrap pl-48">
-						<header class="title justify-end"><svg_page class="icon"/>Extensive Design Templates – Design Made Easy</header>
-						<p class="mt-[12px] text-right">
-							<span class="font-bold">Effortless Creativity:</span>
-							Dive into our vast library of design templates that make it easy for you to create stunning products without any design experience. Personalized Touch: Customize templates to match your style and brand identity, making your merchandise truly yours.
-						</p>
+				<div class="item-box">
+					<header class="title pc:hidden">
+						<svg-icon name="page" class="icon" />Extensive Design Templates – Design Made Easy</header>
+					<div class="item items-start pc:mt-[50px]">
+						<img class="order-2 transform -translate-y-[20px]" src="@/assets/image/pod/growth_2.webp" />
+						<div class="desc-wrap pl-48">
+							<header class="title justify-end mobile:hidden">
+								<svg-icon name="page" class="icon" />Extensive Design Templates – Design Made Easy</header>
+							<p class="desc text-right">
+								<span class="font-bold">Effortless Creativity:</span>
+								Dive into our vast library of design templates that make it easy for you to create stunning products without any design experience. Personalized Touch: Customize templates to match your style and brand identity, making your merchandise truly yours.
+							</p>
+						</div>
 					</div>
 				</div>
 
-				<div class="item items-start mt-[20px]">
-					<img class="transform -translate-y-[60px]" src="@/assets/image/pod/growth_3.webp" />
-					<div class="desc-wrap">
-						<header class="title"><svg_airplane class="icon"/>48 Hours Lightning-Fast Shipping – Start Selling in No Time</header>
-						<p class="mt-[12px]">
-							<span class="font-bold">Quick Turnaround:</span>
-							We handle the production and shipping, ensuring your products are ready to ship within 48 hours. This means you can start selling and profiting almost immediately.
-						</p>
-						<p class="mt-[12px]">
-							<span class="font-bold">Global Shipping:</span>
-							Reach customers worldwide with our reliable shipping solutions, expanding your brand’s reach effortlessly.
-						</p>
+				<div class="item-box">
+					<header class="title pc:hidden">
+						<svg-icon name="airplane" class="icon" />48 Hours Lightning-Fast Shipping – Start Selling in No Time</header>
+					<div class="item items-start pc:mt-[20px]">
+						<img class="transform -translate-y-[60px]" src="@/assets/image/pod/growth_3.webp" />
+						<div class="desc-wrap">
+							<header class="title mobile:hidden"><svg-icon name="airplane" class="icon" />48 Hours Lightning-Fast Shipping – Start Selling in No Time</header>
+							<p class="desc">
+								<span class="font-bold">Quick Turnaround:</span>
+								We handle the production and shipping, ensuring your products are ready to ship within 48 hours. This means you can start selling and profiting almost immediately.
+							</p>
+							<p class="desc">
+								<span class="font-bold">Global Shipping:</span>
+								Reach customers worldwide with our reliable shipping solutions, expanding your brand’s reach effortlessly.
+							</p>
+						</div>
 					</div>
 				</div>
 
-				<div class="item items-start pt-[20px] -mt-[46px] -mb-[30px]">
-					<img class="order-2 transform -translate-y-[20px]" src="@/assets/image/pod/growth_4.webp" />
-					<div class="desc-wrap pl-48">
-						<header class="title justify-end"><svg_globe class="icon"/>Flexible Options, Exceptional Service</header>
-						<p class="mt-[12px] text-right">
-							<span class="font-bold">Hyinsight prioritizes exceptional customer service,</span>
-							offering tailored, customizable solutions for a seamless and personalized experience. - From labeled product to custom packaging, 7*24H service Hyinsight’s flexible options scale with your growing business.
-						</p>
+				<div class="item-box">
+					<header class="title pc:hidden">
+						<svg-icon name="globe" class="icon" />Flexible Options, Exceptional Service</header>
+					<div class="item items-start pc:pt-[20px] pc:-mt-[46px] pc:-mb-[30px]">
+						<img class="order-2 transform -translate-y-[20px]" src="@/assets/image/pod/growth_4.webp" />
+						<div class="desc-wrap pl-48">
+							<header class="title justify-end mobile:hidden"><svg-icon name="globe" class="icon" />Flexible Options, Exceptional Service</header>
+							<p class="desc text-right">
+								<span class="font-bold">Hyinsight prioritizes exceptional customer service,</span>
+								offering tailored, customizable solutions for a seamless and personalized experience. - From labeled product to custom packaging, 7*24H service Hyinsight’s flexible options scale with your growing business.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -123,11 +141,6 @@
 </template>
 
 <script lang="ts" setup>
-import svg_gift from '@/assets/svg/gift.svg'
-import svg_page from '@/assets/svg/page.svg'
-import svg_airplane from '@/assets/svg/airplane.svg'
-import svg_globe from '@/assets/svg/globe.svg'
-
 import img_ladder_1 from '@/assets/image/pod/ladder_1.webp'
 import img_ladder_2 from '@/assets/image/pod/ladder_2.webp'
 import img_ladder_3 from '@/assets/image/pod/ladder_3.webp'
@@ -162,6 +175,7 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 
 <style scoped lang="scss">
 .pod-wrap {
+	//overflow-x: hidden;
 	.video-demo {
 		width: 100%;
 		//cursor: pointer;
@@ -174,6 +188,11 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 			& + .ladder_item {
 				margin-top: 30px;
 			}
+			header {
+				font-size: 24px;
+				font-weight: 600;
+				margin-bottom: 24px;
+			}
 			.desc {
 				font-size: 16px;
 				max-width: 570px;
@@ -185,11 +204,6 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 				height: 220px;
 				margin-right: 60px;
 			}
-			header {
-				font-size: 24px;
-				font-weight: 600;
-				margin-bottom: 24px;
-			}
 			& .ladder_item {
 				margin-top: 30px;
 			}
@@ -199,6 +213,7 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 	.services-wrap {
 		display: flex;
 		flex-direction: column;
+		margin-top: 30px;
 		.item {
 			display: flex;
 			align-items: center;
@@ -215,11 +230,27 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 				//font-style: normal;
 				font-weight: 700;
 			}
+			.desc {
+				margin-top: 16px;
+				margin-bottom: 40px;
+			}
 		}
 	}
 
 	.growth-wrap {
 		color: var(--secondary-color);
+		.title {
+			display: flex;
+			align-items: center;
+			font-size: 16px;
+			//font-style: normal;
+			font-weight: 700;
+			.icon {
+				flex-shrink: 0;
+				margin-right: 6px;
+				font-size: 24px;
+			}
+		}
 		.item {
 			display: flex;
 			//align-items: center;
@@ -229,15 +260,114 @@ const localJump = () => jumpUrl('https://seller.hyinsight.com/#/home/searchResul
 				flex-shrink: 0;
 				//border-radius: 8px;
 			}
+			.desc {
+				margin-top: 12px;
+			}
+		}
+	}
+}
+// 兼容手机端
+.pro_mobile {
+	.pod-wrap {
+		font-size: mx2vw(14px);
+		// 阶梯
+		.ladder {
+			&_item {
+				padding-left: 0 !important;
+				flex-direction: column;
+				& + .ladder_item {
+					margin-top: mx2vw(24px);
+				}
+				header {
+					font-size: mx2vw(16px);
+					margin: mx2vw(12px) 0;
+				}
+				.desc {
+					//font-size: mx2vw(14px);
+					//max-width: 570px;
+					&-wrap {
+						//header {
+						//	margin:
+						//}
+					}
+					& + .desc {
+						margin-top: mx2vw(12px);
+					}
+				}
+				img {
+					height: mx2vw(86px);
+					margin-right: 0;
+					//margin-bottom: mx2vw(12px);
+				}
+			}
+		}
+		// 服务
+		.services {
+			/*&-tip {
+				font-size: mx2vw(14px);
+			}*/
+			&-wrap {
+				margin-top: mx2vw(24px);
+				.item {
+					flex-direction: column;
+					gap: 0;
+					//& + .item {}
+					&-2 {
+						margin-top: mx2vw(36px);
+						img {
+							order: 0;
+						}
+					}
+					img {
+						width: mx2vw(343px);
+					}
+					.title {
+						font-size: mx2vw(16px);
+					}
+					.desc {
+						margin-top: mx2vw(8px);
+						margin-bottom: mx2vw(24px);
+					}
+				}
+			}
+		}
+
+		.growth-wrap {
 			.title {
-				display: flex;
-				align-items: center;
-				font-size: 16px;
-				//font-style: normal;
-				font-weight: 700;
+				//align-items: flex-start;
+				font-size: mx2vw(14px);
+				margin-bottom: mx2vw(16px);
 				.icon {
-					flex-shrink: 0;
-					margin-right: 6px;
+					//flex-shrink: 0;
+					margin-right: mx2vw(6px);
+					font-size: mx2vw(24px);
+				}
+			}
+			.item {
+				//flex-direction: column;
+				gap: 0;
+				align-items: flex-start;
+
+				&-box {
+					margin-bottom: mx2vw(36px);
+					&:last-child {
+						margin-bottom: 0;
+					}
+				}
+				img {
+					width: mx2vw(114px);
+					transform: unset;
+				}
+				.desc {
+					font-size: mx2vw(12px);
+					margin-top: 0;
+					text-align: left;
+					&-wrap {
+						padding-left: 0;
+					}
+					& + .desc {
+						margin-top: mx2vw(10px);
+					}
 				}
 			}
 		}
